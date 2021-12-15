952f6b1ac4a8c776db195ecaa22859f49cb00f9c
abigailjy90 <70828921+abigailjy90@users.noreply.github.com>
2021-12-15 15:33:32 +0900

    add selectors

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,12 +1,28 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
+
+type categories = "TO_DO" | "DOING" | "DONE";
 
 export interface IToDo {
   text: string;
   id: number;
-  category: "TO_DO" | "DOING" | "DONE";
+  category: categories;
 }
+
+export const categoryState = atom<categories>({
+  key: "category",
+  default: "TO_DO",
+});
 
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
+});
+
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    const category = get(categoryState);
+    return toDos.filter((toDo) => toDo.category === category);
+  },
 });
--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -10,7 +10,7 @@ function ToDo({ text, category, id }: IToDo) {
     } = event;
     setToDos((oldToDos) => {
       const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
-      const oldToDo = oldToDos[targetIndex];
+      //   const oldToDo = oldToDos[targetIndex];
       const newToDo = { text, id, category: name as any };
       return [
         ...oldToDos.slice(0, targetIndex),
